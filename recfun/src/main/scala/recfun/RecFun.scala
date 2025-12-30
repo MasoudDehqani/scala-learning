044ba61e0b5bff3366e0d8b712c45e878ce75fd9
Masoud <masoud67.md@gmail.com>
2025-12-30 21:24:59 +0330

    better version of pascal algorithm added to recfun of coursera

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -44,11 +44,24 @@ object RecFun extends RecFunInterface:
   //     val keyTopLeft = (column - 1).toString() + (row - 1).toString()
   //     val keyTopRight = column.toString() + (row - 1).toString()
   //     if row < 2 || (column < 1 || column >= row)
-  //       then 1
+  //     then 1
   //     else
-  //       memo.getOrElseUpdate(keyTopRight, aux(column, row - 1)) + memo.getOrElseUpdate(keyTopLeft, aux(column - 1, row - 1))
+  //       memo.getOrElseUpdate(keyTopRight, aux(column, row - 1)) + memo
+  //         .getOrElseUpdate(keyTopLeft, aux(column - 1, row - 1))
 
   //   aux(c, r)
+
+  def pascalMemo(c: Int, r: Int): Int =
+    val memo = collection.mutable.Map[(Int, Int), Int]()
+
+    def aux(column: Int, row: Int): Int =
+      memo.getOrElseUpdate(
+        (column, row),
+        if row < 2 || column < 1 || column >= row then 1
+        else aux(column, row - 1) + aux(column - 1, row - 1)
+      )
+
+      aux(c, r)
 
   /** Exercise 2
     */
